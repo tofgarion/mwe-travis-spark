412b41ee04b3fec2f24b14d712ce2cb09aea9d05
Christophe Garion <tofgarion@runbox.com>
2018-07-04 14:03:58 +0200

    try to use GNAT Community 2018 with new script

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-# install Z3 4.6.0
-wget https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x64-ubuntu-14.04.zip
-unzip z3-4.6.0-x64-ubuntu-14.04.zip
+# download GNAT Community 2018
+wget http://mirrors.cdn.adacore.com/art/5b0d7bffa3f5d709751e3e04 -O gnat-community-2018
 
-# install CVC4 1.5
-wget http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.5-x86_64-linux-opt -O cvc4
-chmod 755 cvc4
-
-# install SPARK Discovery
-wget http://mirrors.cdn.adacore.com/art/592c5299c7a447388d5c991d -O spark-discovery-gpl-2017-x86_64-linux-bin.tar.gz
-tar zxvf spark-discovery-gpl-2017-x86_64-linux-bin.tar.gz
+# download script to install GNAT Community 2018
+git clone https://github.com/AdaCore/gnat_community_install_script
